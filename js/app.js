07fb313fe8e0dd347818a740b6f17f1ba5981b50
Muhammadzoir Mahmudov <kolen1470@gmail.com>
2024-02-27 22:19:40 +0500

    fixed code

--- js/app.js
+++ js/app.js
@@ -5,29 +5,22 @@ const overlay = document.getElementById('overlay')
 
 const addHiden = () => {
     modal.classList.add('hidden')
-    overley.classList.add('hidden')
+    overlay.classList.add('hidden')
   }
-
-
-showBtn.addEventListener('click' ,() => {
+const removeHidden = () => {
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden')
-
-})
-
-closeBtn.addEventListener('click' ,() => {
-   addHiden()
+}
+showBtn.addEventListener('click' ,removeHidden)/*() => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+})*/
+closeBtn.addEventListener('click' ,addHiden)
    /* modal.classList.add('hidden')
     overlay.classList.add('hidden') */
-    
-})
-
-overlay.addEventListener('click' ,() => {
-    addHiden()
+overlay.addEventListener('click' , addHiden)
     /*modal.classList.add('hidden')
     overlay.classList.add('hidden') */
-    
-})
 
 document.addEventListener('keydown', (e) => {
     if(e.key == "Escape") {
